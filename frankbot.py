45b29debd743fc86cc575e4d66a80018ebe795d4
Caleb Siow <caleb.siow0228@gmail.com>
2022-10-27 22:26:00 -0700

    bot file and flask file

---
+++ frankbot.py
@@ -0,0 +1,37 @@
+import os
+import openai
+from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+completion = openai.Completion()
+
+start_sequence = "\nFrank:"
+restart_sequence = "\n\nPerson:"
+
+session_prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:"
+
+
+
+def ask(question, chat_log = None):
+    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt_text,
+        temperature=0.8,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.3,
+        stop=["\n"]
+        )
+    story = response['choices'][0]['text']
+    return str(story)
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None: 
+        chat_log = session_prompt 
+        return f'{chat_log}{restart_sequence} {question}{start_sequence} {answer}'
+
