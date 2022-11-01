a2e6b23648ee19863bf927c9c977e600a8dde46f
Caleb Siow <caleb.siow0228@gmail.com>
2022-11-01 10:55:39 -0700

    changed param

--- frankbot.py
+++ frankbot.py
@@ -18,13 +18,13 @@ session_prompt = "The following is a conversation with an AI assistant. The assi
 def ask(question, chat_log = None):
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
-        model="text-davinci-002",
+        model="davinci",
         prompt=prompt_text,
         temperature=0.8,
-        max_tokens=150,
+        max_tokens=256,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0.3,
+        presence_penalty=0.1,
         stop=["\n"]
         )
     story = response['choices'][0]['text']
